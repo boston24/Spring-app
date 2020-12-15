a1ae4201876ab7340563832895a00b49010e9ef4
Mateusz Jendernal <matij99@gmail.com>
2020-12-15 22:03:49 +0100

    finished styling

--- src/main/java/com/javaEE/project/controller/ApplicationController.java
+++ src/main/java/com/javaEE/project/controller/ApplicationController.java
@@ -106,6 +106,7 @@ public class ApplicationController {
         pm.removeAppFromList(app,p);
         model.addAttribute("persons",pm.getAllPersonsInApp(id));
         model.addAttribute("id",id);
+        model.addAttribute(am.getUserCountryData(id));
         return "app-removeUsers";
     }
 
