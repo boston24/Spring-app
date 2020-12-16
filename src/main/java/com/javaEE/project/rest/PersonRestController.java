a295216ced772f764206db42b9afb7e001676258
Mateusz Jendernal <matij99@gmail.com>
2020-12-16 09:45:16 +0100

    added searching user by username in rest api

--- src/main/java/com/javaEE/project/rest/PersonRestController.java
+++ src/main/java/com/javaEE/project/rest/PersonRestController.java
@@ -6,6 +6,7 @@ import com.javaEE.project.service.PersonManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,10 +25,11 @@ public class PersonRestController {
     }
 
     @GetMapping("/display")
-    public List<HashMap> getAll(){
+    public List<HashMap> getAll(@RequestParam(required = false) String username){
         List<HashMap> out = new ArrayList<>();
 
-        for(Person person : pm.getAllPersons()) {
+        if(username!=null){
+            Person person = pm.getPersonByUsername(username);
             HashMap per = new HashMap();
             per.put("first_name", person.getFirst_name());
             per.put("last_name", person.getLast_name());
@@ -37,6 +39,19 @@ public class PersonRestController {
             per.put("password", person.getPassword());
             per.put("aplications", pm.getAppNames(person.getId()));
             out.add(per);
+            }
+        else{
+            for(Person person2 : pm.getAllPersons()){
+                HashMap per = new HashMap();
+                per.put("first_name", person2.getFirst_name());
+                per.put("last_name", person2.getLast_name());
+                per.put("email", person2.getEmail());
+                per.put("country", person2.getCountry());
+                per.put("username", person2.getUsername());
+                per.put("password", person2.getPassword());
+                per.put("aplications", pm.getAppNames(person2.getId()));
+                out.add(per);
+            }
         }
             return out;
     }
