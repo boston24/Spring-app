7ca62520d3ca18ec3ad7261da46b17b05dda6ca9
Mateusz Jendernal <matij99@gmail.com>
2021-01-13 18:59:29 +0100

    added configuration for posgres and modified models to work with db

--- src/main/java/com/javaEE/project/domain/Application.java
+++ src/main/java/com/javaEE/project/domain/Application.java
@@ -4,7 +4,11 @@ import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import scala.tools.nsc.doc.base.comment.Table;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -14,22 +18,28 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="application")
 public class Application {
 
+    @Id
     @CsvBindByName
     private String id;
 
+    @Column(name="name")
     @CsvBindByName(column = "Aplication name")
     @NotNull(message = "App name is required")
     @Size(min = 1, message = "Name is required")
     private String name;
 
+    @Column(name="domain")
     @CsvBindByName(column = "Domain name")
     @NotNull(message = "Domain is required")
     @Size(min = 1, message = "Domain is required")
     @Pattern(regexp = "^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\\.[a-zA-Z]{2,}$", message = "Required format example: domainname.com")
     private String domain;
 
+    @Column(name="users")
     private List<Person> user_list = new ArrayList<>();
 
 }
--- src/main/java/com/javaEE/project/domain/Person.java
+++ src/main/java/com/javaEE/project/domain/Person.java
@@ -4,7 +4,11 @@ import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import scala.tools.nsc.doc.base.comment.Table;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -14,43 +18,53 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="person")
 public class Person {
 
+    @Id
     @CsvBindByName
     private String id;
 
+    @Column(name="first_name")
     @CsvBindByName
     @NotNull(message = "Name is required")
     @Size(min = 1, message = "Name is required")
     //@Pattern(regexp = "[A-Za-z]")
     private String first_name;
 
+    @Column(name="last_name")
     @CsvBindByName
     @NotNull(message = "Last name is required")
     //@Pattern(regexp = "[A-Za-z]")
     @Size(min = 1, message = "Last name is required")
     private String last_name;
 
+    @Column(name="email")
     @CsvBindByName
     @NotNull(message = "Email is required")
     @Pattern(regexp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$", message = "Required email example: xyz@site.com")
     private String email;
 
+    @Column(name="country")
     @CsvBindByName(column = "Country")
     @NotNull(message = "Country required")
     @Pattern(regexp = "[a-zA-Z]{2,}", message = "Only letters of the alphabet allowed")
     private String country;
 
+    @Column(name="username")
     @CsvBindByName(column = "Username")
     @NotNull(message = "Username required")
     @Size(min = 1, message = "Username is required")
     private String username;
 
+    @Column(name = "password")
     @CsvBindByName(column = "Password")
     @NotNull(message = "Password required")
     @Size(min = 6, message = "Password too short (min. 6 characters)")
     private String password;
 
+    @Column(name = "apps")
     private List<Application> app_list = new ArrayList<>();
 
 }
