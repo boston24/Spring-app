29a8a68a4ff1ba8332c85bb5093660c0bc66eb34
Mateusz Jendernal <matij99@gmail.com>
2021-01-20 00:36:40 +0100

    added page for unauthorized users

--- src/main/java/com/javaEE/project/controller/HomeController.java
+++ src/main/java/com/javaEE/project/controller/HomeController.java
@@ -14,6 +14,7 @@ import com.sun.mail.iap.ConnectionException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailAuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -122,6 +123,12 @@ public class HomeController {
         }
         model.addAttribute("apps", am.getAllApplications());
         return "all/allApps";
+    }
+
+    @GetMapping("/accessDenied")
+    public String denied(){
+        SecurityContextHolder.clearContext();
+        return "accessDenied";
     }
 
 }
--- src/main/java/com/javaEE/project/security/SecurityConfiguration.java
+++ src/main/java/com/javaEE/project/security/SecurityConfiguration.java
@@ -7,12 +7,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -33,8 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and()
                 .formLogin()
-                //.and()
-                //.exceptionHandling().accessDeniedPage("")
+                .and()
+                .exceptionHandling().accessDeniedPage("/accessDenied")
                 .and()
                 .logout().permitAll()
                 .logoutSuccessUrl("/");
